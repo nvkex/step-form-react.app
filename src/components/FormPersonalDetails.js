92ca3ea2250ced41116cd7f10752b26a3749c0a5
nvkex <scrbwrm@gmail.com>
2020-08-06 22:45:04 +0530

    Personal Details Components

---
+++ src/components/FormPersonalDetails.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RadioButton from 'material-ui/RadioButton'
+import { RaisedButton } from 'material-ui';
+
+class FromPersonalDetails extends Component {
+
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();
+    }
+
+    render() {
+
+        const { values, handleChange } = this.props;
+
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Enter Personal Details" />
+                    <TextField
+                        hintText="Enter Your Occupations"
+                        floatingLabelText="Cccupation"
+                        onChange={handleChange('occupation')}
+                        defaultValue={values.occupation}
+                    /><br />
+                    <TextField
+                        hintText="Enter Your City"
+                        floatingLabelText="City"
+                        onChange={handleChange('city')}
+                        defaultValue={values.city}
+                    /><br />
+                    <TextField
+                        hintText="Enter Your Bio"
+                        floatingLabelText="Bio"
+                        onChange={handleChange('bio')}
+                        defaultValue={values.bio}
+                    /><br />
+                    <RaisedButton
+                        label="Continue"
+                        primary={true}
+                        style={styles.button}
+                        onClick={this.continue}
+                    />
+                    <RaisedButton
+                        label="Back"
+                        primary={false}
+                        style={styles.button}
+                        onClick={this.back}
+                    />
+
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+const styles = {
+    button: { margin: 15 }
+}
+
+export default FromPersonalDetails
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FormUserDetails from './FormUserDetails';
+import FromPersonalDetails from './FormPersonalDetails';
 
 class UserForm extends Component {
 
@@ -45,7 +46,14 @@ class UserForm extends Component {
                     />
                 );
             case 2:
-                return <h1>FormPersonalDetails</h1>
+                return (
+                    <FromPersonalDetails
+                        nextStep={this.nextStep}
+                        prevStep={this.prevStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                )
             case 3:
                 return <h1>Confirm</h1>
             case 4:
